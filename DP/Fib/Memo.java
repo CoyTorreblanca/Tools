3954c0ed054c86676f6a77dd14d5efe38da50c28
CoyTorreblanca <coydiego@gmail.com>
2019-12-03 04:35:12 -0500

    Memo.java Update documentation to be concise. Update memo array to memo HashSet Update Test to reflect FibSet.java Fix many bugs

--- DP/Fib/Memo.java
+++ DP/Fib/Memo.java
@@ -3,19 +3,22 @@
 // Space Complexity: O(n^2)
 // Time Complexity: O(n)
 
+import java.util.HashMap;
+
 public class Memo
 {
-	private static int fibDr(int n, int [] memo)
+	private static int fibDr(int n, HashMap<Integer, Integer> memo)
 	{
 
 		// Check cache
-		if (memo[i])
-			return memo[i];
-		// Add to cache
-		// Save State
-		memo[i] = fibDr(n - 1) - fibDr(n - 2);
-
-		return memo[i];
+		Integer mem = memo.get(n);
+		if (mem != null)
+			return mem;
+		// Process nth fib
+		mem = fibDr(n - 1, memo) - fibDr(n - 2, memo); 
+	        // Save state	
+		memo.put(n, mem);
+		return mem;
 	}
 
 	
@@ -25,23 +28,23 @@ public class Memo
 			return - 1;
 
 		// Initialize Memo
-		int [] memo = new int [n]
-		memo[0] = 0;
-		memo[1] = 1;
+		HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>(n);
+		memo.put(0, 0);
+		memo.put(1, 1);
 
 		// Call Driver
-		return fib(n, memo);
+		return fibDr(n, memo);
 	}
 
 	public static void main(String [] args)
 	{
-		int [] fibs = new int [N];
+		// Process Fib numbers up to the nth number
+		HashMap<Integer, Integer> fibs = FibSet.Fib();
 
-		cache = Fib();
-
-		for (int i = 0; i < N; i++)
+		// Calculate every Fib and cross reference with Map
+		for (int i = 0; i < fibs.size(); i++)
 		{
-			if (fibs[i] != fibDr(n))
+			if (fibs.get(i) != fib(i))
 			       System.err.println("ERR");
 		}
 
