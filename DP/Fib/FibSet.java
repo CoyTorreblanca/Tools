ef779cfd978fb0a7856f648b9fcc131d025f30ae
CoyTorreblanca <coydiego@gmail.com>
2019-12-03 04:31:21 -0500

    FibSet.java Read file Fib.txt to produce dataset of first n fibonacci numbers. Fib.txt format: [i] [ith fibonacii number] Skeleton required to allow tests to compile Fixed many bugs

---
+++ DP/Fib/FibSet.java
@@ -0,0 +1,23 @@
+// Coy Torrebalnca
+// Read file Fib.txt to produce a dataset of first N fibs
+
+import java.io.*;
+import java.util.HashMap;
+
+public class FibSet
+{
+	public static final int MAX_N = 2000; // Maximum fib count
+	public static final String FILE_NM = "Fib.txt"; // Datase
+
+	public static HashMap<Integer, Integer> FibDr(int n)
+	{
+		return new HashMap<Integer, Integer>();
+	}
+
+	public static HashMap<Integer, Integer> Fib()
+	{
+		return new HashMap<Integer, Integer>();
+	}
+}
+	
+	
